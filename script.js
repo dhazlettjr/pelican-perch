d6bc3cc87f4aef928862f2e6939300e0a7261abb
David A Hazlett Jr <d.hazlett.jr@gmail.com>
2025-05-14 17:52:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-fetch("https://yourdomain.com/blocked-dates")
-  .then(res => res.json())
-  .then(bookedRanges => {
-    flatpickr("#datePicker", {
-      inline: true,
-      mode: "range",
-      minDate: "today",
-      disable: bookedRanges,
-      onClose: function(selectedDates) {
-        if (selectedDates.length === 2) {
-          const nights = Math.round((selectedDates[1] - selectedDates[0]) / (1000 * 60 * 60 * 24));
-          document.getElementById("paypalNights").value = nights;
-          document.getElementById("paypalAmount").value = nights * 190;
-        }
-      }
-    });
-  });
+flatpickr("#datePicker", {
+  inline: true,
+  mode: "range",
+  minDate: "today",
+  disable: [
+    { from: "2025-05-14", to: "2025-05-16" },
+    { from: "2025-05-20", to: "2025-05-22" }
+  ],
+  onClose: function(selectedDates) {
+    if (selectedDates.length === 2) {
+      const nights = Math.round((selectedDates[1] - selectedDates[0]) / (1000 * 60 * 60 * 24));
+      document.getElementById("paypalNights").value = nights;
+      document.getElementById("paypalAmount").value = nights * 190;
+    }
+  }
+});
